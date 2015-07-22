04e0aa5296cf5192cadb4ea37aac6142b1e081bb
Brian Haberer <bhaberer@gmail.com>
2015-07-21 17:51:55 -0700

    Small sanity pass

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,44 +1,24 @@
 class StationsController < ApplicationController
   before_action :set_station, only: [:show, :edit, :update, :destroy]
 
-  # GET /stations
-  # GET /stations.json
   def index
     @stations = Station.all
   end
 
-  # GET /stations/1
-  # GET /stations/1.json
   def show
   end
 
-  # GET /stations/new
   def new
-    @station = Station.new
+    redirect_to :root, notice: 'Stations are not created this way.'
   end
 
-  # GET /stations/1/edit
   def edit
   end
 
-  # POST /stations
-  # POST /stations.json
   def create
-    @station = Station.new(station_params)
-
-    respond_to do |format|
-      if @station.save
-        format.html { redirect_to @station, notice: 'Station was successfully created.' }
-        format.json { render :show, status: :created, location: @station }
-      else
-        format.html { render :new }
-        format.json { render json: @station.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to :root, notice: 'Stations are not created this way.'
   end
 
-  # PATCH/PUT /stations/1
-  # PATCH/PUT /stations/1.json
   def update
     respond_to do |format|
       if @station.update(station_params)
@@ -51,20 +31,14 @@ class StationsController < ApplicationController
     end
   end
 
-  # DELETE /stations/1
-  # DELETE /stations/1.json
   def destroy
-    @station.destroy
-    respond_to do |format|
-      format.html { redirect_to stations_url, notice: 'Station was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to :root, notice: 'Stations are not removed this way.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_station
-      @station = Station.find(params[:id])
+      @station = Station.find_by_param(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
