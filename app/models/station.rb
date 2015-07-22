b2ff3a46c54432571ecdf786df0c45d75d7ec057
Brian Haberer <bhaberer@gmail.com>
2015-07-21 17:55:56 -0700

    Use uid for params, and set uid for stations on save

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,10 +1,25 @@
 # Class to manage Station information
 class Station < ActiveRecord::Base
   has_many :stops, dependent: :destroy
+  has_many :trains, through: :stops
 
   validates :name, presence: true
   validates :zone, presence: true,
                    numericality: true
   validates :order, presence: true,
                     numericality: true
+
+  after_create :generate_uid
+
+  def generate_uid
+    update_attribute(:uid, name.downcase.gsub(/\s/, '_').gsub(/\W/, ''))
+  end
+
+  def to_param
+    uid
+  end
+
+  def self.find_by_param(input)
+    find_by_uid(input)
+  end
 end
